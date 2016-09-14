947e8099396379f76ec3ef7bed5134c1a3b939b7
ConnorFieldUser <conquest.field@gmail.com>
2016-09-14 12:00:40 -0400

    added lists and dictionary file

--- day3.py
+++ day3.py
@@ -1,3 +1,12 @@
 
 for _ in range(5):
     print("I am looping")
+
+turn = 0
+while turn < 5:
+    print("WHILE LOOPING")
+    turn += 1
+
+choice = ''
+while choice != "n":
+    choice = input("Do you want to loop again? [Y/n] ")
---
+++ day3_collections.py
@@ -0,0 +1,27 @@
+
+my_list = ["connor", 33, ["the dark", "wasps"]]
+
+print(my_list)
+my_list.append("peanut")
+my_list.pop()
+print(my_list)
+print(my_list[2][0])
+
+one_dim = 1
+two_dim = [2, 4, 5]
+three_dim = [[4, 5, 6],
+             [2, 5, 6],
+             [2, 6, 3]]
+
+my_dictionary = {
+    "name": "connor",
+    "another": "test"
+}
+
+
+print(my_dictionary["name"])
+print(my_dictionary["another"])
+
+print(my_dictionary.keys())
+
+# print(help(help))
