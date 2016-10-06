48ecd3886e28721b6a6f79605d4f17e4af56ea3a
ConnorFieldUser <conquest.field@gmail.com>
2016-10-06 11:38:13 -0400

    added url, view, template

--- week4/lyrics/record_store/views.py
+++ week4/lyrics/record_store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+import datetime
+
+from record_store.models import Song
+
+
+def index_view(request):
+    context = {
+        "my_name": "Connor",
+        "now": datetime.datetime.now(),
+        "all_songs": Song.objects.all()
+    }
+    return render(request, 'index.html', context)
