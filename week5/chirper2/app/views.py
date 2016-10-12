13394103bbebc02c19d6b169ca9246accd4c5703
ConnorFieldUser <conquest.field@gmail.com>
2016-10-12 14:03:45 -0400

    notes

--- week5/chirper2/app/views.py
+++ week5/chirper2/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from app.models import Chirp
+from app.forms import ChirpForm
 
 # Create your views here.
 
@@ -8,10 +9,11 @@ from app.models import Chirp
 def index_view(request):
     print(request.POST)
     if request.POST:
-        chirp_body = request.POST["chirp_body"]
-        if chirp_body != "":
-            Chirp.objects.create(body=chirp_body)
+        instance = ChirpForm(request.POST)
+        if instance.is_valid():
+            instance.save()
     context = {
+        "form": ChirpForm(),
         "all_chirps": Chirp.objects.all().order_by("-created")
     }
     return render(request, 'index.html', context)
