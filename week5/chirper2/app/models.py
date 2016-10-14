1d577217b067f7c5f711783b5fd19d38ccefd627
ConnorFieldUser <conquest.field@gmail.com>
2016-10-14 13:55:52 -0400

    broken link

--- week5/chirper2/app/models.py
+++ week5/chirper2/app/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Chirp(models.Model):
     body = models.CharField(max_length=140)
     created = models.DateTimeField(auto_now_add=True)
+    user = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('auth.User')
 
     class Meta:
         ordering = ("-created",)
--- week5/chirper2/app/views.py
+++ week5/chirper2/app/views.py
@@ -5,6 +5,11 @@ from django.views.generic.edit import CreateView, UpdateView
 
 from app.models import Chirp
 from app.forms import ChirpForm
+
+from django.contrib.auth.models import User
+
+from django.contrib.auth.forms import UserCreationForm
+
 # TemplateView
 
 # Create your views here.
@@ -48,8 +53,19 @@ class ChirpCreateView(CreateView):
     success_url = "/chirps"
     fields = ('body',)
 
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        return super().form_valid(form)
+
 
 class ChirpUpdateView(UpdateView):
     model = Chirp
     success_url = "/chirps"
     fields = ('body', )
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/chirps"
