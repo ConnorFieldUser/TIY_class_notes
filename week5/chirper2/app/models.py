023271a8321517c0c51d88ddb708a1edf1484963
ConnorFieldUser <conquest.field@gmail.com>
2016-10-17 10:44:28 -0400

    more notes

--- week5/chirper2/app/models.py
+++ week5/chirper2/app/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class Chirp(models.Model):
     body = models.CharField(max_length=140)
     created = models.DateTimeField(auto_now_add=True)
-    user = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey('auth.User')
 
     class Meta:
