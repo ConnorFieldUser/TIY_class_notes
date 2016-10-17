292f4bae29c0c622ae31710152d4afb4e1920247
ConnorFieldUser <conquest.field@gmail.com>
2016-10-17 11:34:56 -0400

    notes

--- week5/chirper2/app/admin.py
+++ week5/chirper2/app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from app.models import Chirp
+from app.models import Chirp, Vote
 
 # Register your models here.
 
-admin.site.register(Chirp)
+admin.site.register([Chirp, Vote])
--- week5/chirper2/app/views.py
+++ week5/chirper2/app/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView
 
 from app.models import Chirp
-from app.forms import ChirpForm
+# from app.forms import ChirpForm
 
 from django.contrib.auth.models import User
 
@@ -13,19 +13,6 @@ from django.contrib.auth.forms import UserCreationForm
 # TemplateView
 
 # Create your views here.
-
-
-def index_view(request):
-    print(request.POST)
-    if request.POST:
-        instance = ChirpForm(request.POST)
-        if instance.is_valid():
-            instance.save()
-    context = {
-        "form": ChirpForm(),
-        "all_chirps": Chirp.objects.all().order_by("-created")
-    }
-    return render(request, 'index.html', context)
 
 
 def about_view(request):
