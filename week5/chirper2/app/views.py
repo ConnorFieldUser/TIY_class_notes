0977bb53dba886c1f371b547b5d79bf996314f5a
ConnorFieldUser <conquest.field@gmail.com>
2016-10-17 14:06:04 -0400

    Notes

--- week5/chirper2/app/views.py
+++ week5/chirper2/app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView
 
-from app.models import Chirp
+from app.models import Chirp, Vote
 # from app.forms import ChirpForm
 
 from django.contrib.auth.models import User
@@ -37,7 +37,7 @@ class ChirpDetailView(DetailView):
 
 class ChirpCreateView(CreateView):
     model = Chirp
-    success_url = "/chirps"
+    success_url = "/"
     fields = ('body',)
 
     def form_valid(self, form):
@@ -48,11 +48,27 @@ class ChirpCreateView(CreateView):
 
 class ChirpUpdateView(UpdateView):
     model = Chirp
-    success_url = "/chirps"
+    success_url = "/"
     fields = ('body', )
 
 
 class UserCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = "/chirps"
+    success_url = "/"
+
+
+class ChirpVoteView(CreateView):
+    model = Vote
+    success_url = "/"
+    fields = ('value',)
+
+    def form_valid(self, form):
+        try:
+            Vote.objects.get(user=self.request.user, chirp_id=self.kwargs["pk"]).delete()
+        except Vote.DoesNotExist:
+            pass
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        instance.chirp = Chirp.objects.get(pk=self.kwargs["pk"])
+        return super().form_valid(form)
--- week5/chirper2/chirper2/urls.py
+++ week5/chirper2/chirper2/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from app.views import about_view, ChirpView, ChirpDetailView, ChirpCreateView, \
-                      ChirpUpdateView, UserCreateView
+                      ChirpUpdateView, UserCreateView, ChirpVoteView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^', include('django.contrib.auth.urls')),
+    url(r'^chirps/(?P<pk>\d+)/upvote$', ChirpVoteView.as_view(), name="chirp_upvote_view"),
+    url(r'^chirps/(?P<pk>\d+)/downvote$', ChirpVoteView.as_view(), name="chirp_downvote_view"),
     url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view"),
     url(r'^$', ChirpView.as_view(), name="chirp_view"),
     url(r'^chirps/create$', ChirpCreateView.as_view(), name="chirp_create_view"),
