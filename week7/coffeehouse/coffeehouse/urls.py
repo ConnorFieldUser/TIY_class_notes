da143b6901e37989b3c07ffec0ec5a8492cc3b2d
ConnorFieldUser <conquest.field@gmail.com>
2016-10-26 11:57:37 -0400

    notes

--- week7/coffeehouse/coffeehouse/urls.py
+++ week7/coffeehouse/coffeehouse/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from menu_api.views import SpecialListAPIView, SpecialDetailAPIView
+from menu_api.views import SpecialListCreateAPIView, SpecialDetailUpdateDestroyAPIView, IngredientListAPIView, IngredientDetailUpdateDestroyAPIView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^specials/$', SpecialListAPIView.as_view(), name="special_list_api_view"),
-    url('^specials/(?P<pk>\d+)/$', SpecialDetailAPIView.as_view(), name="special_detail_api_view")
+    url(r'^ingredients/$', IngredientListAPIView.as_view(), name="ingredient_list_api_view"),
+    url(r'^ingredients/(?P<pk>\d+)/$', IngredientDetailUpdateDestroyAPIView.as_view(), name="ingredient_detail_update_destroy_api_view"),
+    url(r'^specials/$', SpecialListCreateAPIView.as_view(), name="special_list_create_api_view"),
+    url(r'^specials/(?P<pk>\d+)/$', SpecialDetailUpdateDestroyAPIView.as_view(), name="special_detail_update_destroy_api_view")
 ]
