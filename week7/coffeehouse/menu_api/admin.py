a9b7b3e9ddd58c2cd1c3076a8260386b47711692
ConnorFieldUser <conquest.field@gmail.com>
2016-10-27 12:05:08 -0400

    commit

--- week7/coffeehouse/menu_api/admin.py
+++ week7/coffeehouse/menu_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# from menu_api import Special
+#
+# # Register your models here.
+#
+# admin.site.register(Special)
--- week7/coffeehouse/menu_api/views.py
+++ week7/coffeehouse/menu_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 
 from menu_api.models import Special, Ingredient
 from menu_api.serializers import SpecialSerializer, IngredientSerializer
+from menu_api.permissions import IsCreatedByOrReadOnly
 
 # Create your views here.
 
@@ -11,10 +12,17 @@ class SpecialListCreateAPIView(ListCreateAPIView):
     queryset = Special.objects.all()
     serializer_class = SpecialSerializer
 
+    def perform_create(self, serializer):
+        # print(serializer)
+        # print(serializer.data)
+        serializer.save(created_by=self.request.user)
+        return super().perform_create(serializer)
+
 
 class SpecialDetailUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Special.objects.all()
     serializer_class = SpecialSerializer
+    permission_classes = (IsCreatedByOrReadOnly,)
 
 
 class IngredientListAPIView(ListCreateAPIView):
