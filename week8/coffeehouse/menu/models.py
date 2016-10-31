240f581430773ce6241374019e5a57dea98d74a2
ConnorFieldUser <conquest.field@gmail.com>
2016-10-31 10:54:30 -0400

    commit

---
+++ week8/coffeehouse/menu/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Special(models.Model):
+    created_user = models.ForeignKey('auth.user')
+    title = models.CharField(max_length=50),
+    description = models.TextField(),
+    created_times = models.DateTimeField(auto_now_add=True)
