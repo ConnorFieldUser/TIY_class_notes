48b80eb0edec65a3d8ccd57bf082a7196fba5b6c
ConnorFieldUser <conquest.field@gmail.com>
2016-10-31 11:02:49 -0400

    commit

--- week8/coffeehouse/menu/models.py
+++ week8/coffeehouse/menu/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Special(models.Model):
     created_user = models.ForeignKey('auth.user')
-    title = models.CharField(max_length=50),
-    description = models.TextField(),
+    title = models.CharField(max_length=50)
+    description = models.TextField()
     created_times = models.DateTimeField(auto_now_add=True)
+    picture = models.FileField()
