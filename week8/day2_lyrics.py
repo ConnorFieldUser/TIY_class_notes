44d548968674c9940ab2e3759af2219c46c46734
ConnorFieldUser <conquest.field@gmail.com>
2016-11-01 12:59:31 -0400

    notes

--- week8/day2_lyrics.py
+++ week8/day2_lyrics.py
@@ -1,6 +1,5 @@
 import requests
 
-import time
 
 from bs4 import BeautifulSoup
 
@@ -17,13 +16,28 @@ souper = BeautifulSoup(body.text, "html.parser")
 all_a_tags = (souper.findAll("a"))
 next_page = all_a_tags[28].get("href")
 
-time.sleep(2)
-
 
 # ############### All Songs
 body = requests.get(next_page, headers=headers)
-print(body.text)
+souper = BeautifulSoup(body.text, "html.parser")
+all_a_tags = souper.findAll("a")[33:290]
+song_link_list = []
+for counter, tag in enumerate(all_a_tags):
+    if not tag.get("rel") and tag.get("href"):
+        song_link_list.append(tag.get("href"))
 
+for song_link in song_link_list:
+    new_song_link = song_link.replace("..", "http://www.azlyrics.com")
+    song_page = requests.get(new_song_link, headers=headers)
+    souper = BeautifulSoup(song_page.text, "html.parser")
+    all_divs = souper.findAll("div")
+    song_div = all_divs[22]
+    # for counter, div in enumerate(all_divs):
+    #     print(counter, div)
+    print(song_div.text)
+    input()
+
+# print(song_link_list)
 
 # for counter, tag in enumerate(all_a_tags):
 #     print(tag)
